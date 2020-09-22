47f030d5636a6b61c9bc9868e3479e7027425a8d
ashaynaik <ashaydnaik@gmail.com>
2020-09-22 19:43:01 +1000

    Add Learning

---
+++ Core Python/Learning/learn_modules/wo.py
@@ -0,0 +1,5 @@
+"""wo module. Contains function: words_occur()"""
+# interface functions
+def words_occur():
+    """words_occur() - count the occurrences of words in a file."""
+# Prompt user for the name of the file to use.
